59a589243ce3768abbd5a7dfaf190985fc59dc02
DornMira <111016349+DornMira@users.noreply.github.com>
2023-03-15 03:41:13 +0300

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-// Concat two files
-
 const fs = require('fs');
-
-const data1 = fs.readFileSync(process.argv[2], 'utf8');
-const data2 = fs.readFileSync(process.argv[3], 'utf8');
-
-fs.writeFileSync(process.argv[4], data1 + data2);
+const src1 = fs.readFileSync(process.argv[2], 'utf8');
+const src2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], src1 + src2);
